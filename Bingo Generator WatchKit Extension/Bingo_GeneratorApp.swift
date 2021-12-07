16acd40d4c5323fd9ac30dcfb8d78411a9828c17
UNIVAC-Coder <82778944+UNIVAC-Coder@users.noreply.github.com>
2021-12-07 15:24:12 -0500

    Added CC BY 4.0 and copyright; next push to GitHub

--- Bingo Generator WatchKit Extension/Bingo_GeneratorApp.swift
+++ Bingo Generator WatchKit Extension/Bingo_GeneratorApp.swift
@@ -1,8 +1,14 @@
 //
 //  Bingo_GeneratorApp.swift
 //  Bingo Generator WatchKit Extension
+//  Creative Commons Attribution 4.0 International Public License
+//  Creative Commons may be contacted at creativecommons.org.
 //
-//  Created by Tom Cavalli on 12/2/21.
+//  Copyright (c) 2021 Thomas Cavalli
+//  Thomas Cavalli may be contacted at thomascavalli.com
+//
+//  List of What Changed (by Who and When):
+//  Created by Thomas Cavalli on 12/2/21.
 //
 
 import SwiftUI
@@ -15,7 +21,5 @@ struct Bingo_GeneratorApp: App {
                 ContentView()
             }
         }
-
- //       WKNotificationScene(controller: NotificationController.self, category: "myCategory")
     }
 }
